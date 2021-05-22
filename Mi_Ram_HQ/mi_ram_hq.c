4fb0d5fb02fda645ca8c522404a7030378908bcc
SForlizzi <63567466+SForlizzi@users.noreply.github.com>
2021-05-22 20:54:45 -0300

    4/4

--- Mi_Ram_HQ/mi_ram_hq.c
+++ Mi_Ram_HQ/mi_ram_hq.c
@@ -11,8 +11,11 @@ int main(int argc, char** argv){
 
 	logger = log_create("mi_ram_hq.log", "MI_RAM_HQ", 1, LOG_LEVEL_DEBUG);
 	config = config_create("mi_ram_hq.config");
-	config_discordiador = config_create("../Discordiador/discordiador.config");
-	
+	//config_discordiador = config_create("../Discordiador/discordiador.config");
+
+	int tamanio_memoria = config_get_string_value(config, "TAMANIO_MEMORIA");
+	char* memoria = malloc(tamanio_memoria);
+
     int mi_ram_fd = iniciar_servidor();
 
     int discordiador_fd = esperar_discordiador(mi_ram_fd);
@@ -44,6 +47,7 @@ int main(int argc, char** argv){
     log_destroy(logger);
     config_destroy(config);
     config_destroy(config_discordiador);
+    free(memoria);
 	return EXIT_SUCCESS;
 
     /*pthread_t hilo_escucha;
@@ -51,6 +55,50 @@ int main(int argc, char** argv){
 
 	pthread_join(hilo_escucha, NULL);*/
 }
+
+t_patota* iniciar_patota(FILE* archivo){
+	t_PCB* pcb = malloc(sizeof(t_PCB));
+	pcb->PID = nuevo_pid();//TODO A discusión de como sacar el pid
+	pcb->direccion_tareas = &archivo;
+
+	t_patota* patota;
+	patota->archivo_de_tareas = archivo;
+	patota->pcb = pcb;
+
+	//cargar_en_Mongo(archivo);
+
+	return patota;
+}
+
+int nuevo_pid(){
+	int id_patota = 1;
+	while(1){
+		if(!existe(id_patota)) {
+	    	return id_patota;
+	    }
+	    id_patota++;
+	}
+}
+
+//Iniciar tripulante: será el encargado de crear la o las estructuras
+//administrativas necesarias para que un tripulante pueda ejecutar.
+
+t_tripulante* iniciar_tripulante(char* posicion, t_PCB* puntero_pcb, int tid){
+	t_TCB* tcb = malloc(sizeof(t_TCB));
+	tcb->TID = tid;
+	tcb->estado_tripulante = LLEGADA; //Supongo que se inicializa en LLEGADA por defecto
+	tcb->coord_x = posicion[0];
+	tcb->coord_y = posicion[2];
+	//tcb->siguiente_instruccion = ;//Ni idea de que va acá
+	tcb->puntero_a_pcb = puntero_pcb;
+
+	t_tripulante* tripulante;
+	//tripulante->codigo = ;//Ni idea de que va acá
+	tripulante->tcb = tcb;
+	return tripulante;
+}
+
+
 
 /*
 void escuchar_alos_cliente(){
