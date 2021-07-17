deedde1d4b288e9a51dfdd9a75b95f4abc4ba175
Javo656 <jirungarayortiz@frba.utn.edu.ar>
2021-07-17 01:03:54 -0300

    Arreglado agregar muchos recursos

--- I_Mongo_Store/i_mongo_store.c
+++ I_Mongo_Store/i_mongo_store.c
@@ -40,14 +40,14 @@ int main(int argc, char** argv){
 
     // log_warning(logger_mongo, "Verifiquemos los blocks");
 	// reparar();
-/*
-    int reparado = verificar_blocks();
+
+    int reparado = verificar_sizes();
 
 	if (reparado){
 		log_warning(logger_mongo, "Se repara la lista de bloques de los recursos");
 	}
     log_warning(logger_mongo, "lOGRADO");
-*/
+
 
 	// Se crean los mutexs de los distintos archivos que se alteran, bitacoras no necesitan por ser propias a cada tripulante (puede que se requiera un mutex para la lista)
 	pthread_mutex_init(&mutex_oxigeno, NULL);
